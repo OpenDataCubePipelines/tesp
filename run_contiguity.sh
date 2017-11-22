981b169d5a1f74fb9783aa544703f0109783e280
Josh Sixsmith <joshua.sixsmith@ga.gov.au>
2017-11-22 17:01:00 +1100

    Modifications for filename changes. Create specific resolution vrts.

--- run_contiguity.sh
+++ run_contiguity.sh
@@ -7,6 +7,23 @@
 #                                                                                                                #
 ##################################################################################################################
 
-for i in `ls $1`; do for j in `ls $1/$i`; do k=`find $1/$i/$j -name 'BAND*tif' | grep $3`; gdalbuildvrt -resolution user -tr 20 20 -separate -overwrite `echo $2/$j.vrt | sed -e "s/L1C/$3/g"` $k; done; done
+cwd=$PWD
 
-for l in `find $2 -name *NBART*vrt`; do python contiguity.py $l --output $2/;  done
+for i in `ls $1`; do for j in `ls $1/$i`; do k=`find $1/$i/$j/$3/ -wholename '*TIF' | grep $3`; gdalbuildvrt -resolution user -tr 20 20 -separate -overwrite `echo $2/$j\_ALLBANDS_20m.vrt | sed -e "s/ARD/$3/g"` $k; done; done
+
+for l in `find $2 -name "*$3_*vrt"`; do python contiguity.py $l --output $2/;  done
+
+for i in `ls $1`; do for j in `ls $1/$i`; do cd $1/$i/$j/$3; gdalbuildvrt -separate -overwrite `echo $j\_BANDS_10m.vrt | sed -e "s/ARD/$3/g"` *_B0[2-48].TIF; gdalbuildvrt -separate -overwrite `echo $j\_BANDS_20m.vrt | sed -e "s/ARD/$3/g"` *_B0[5-7].TIF *_B8A.TIF *_B1[0-1].TIF; gdalbuildvrt -separate -overwrite `echo $j\_BANDS_60m.vrt | sed -e "s/ARD/$3/g"` *_B01.TIF *_B09.TIF; cd $cwd; done; done
+
+# below for non gurus :)
+
+# for i in `ls $1`
+#     do for j in `ls $1/$i`
+#         do k=`find $1/$i/$j/$3/ -wholename '*TIF'`
+#         gdalbuildvrt -resolution user -tr 20 20 -separate -overwrite `echo $2/$j\_ALLBANDS_20m.vrt | sed -e "s/ARD/$3/g"` $k
+#     done
+# done
+
+# for l in `find $2 -name "*$3_*vrt"`
+#     do python contiguity.py $l --output $2/
+# done
