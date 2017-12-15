a8b194377a107009a1b9e2cefa72543c69c34095
Josh Sixsmith <josh.sixsmith@gmail.com>
2017-12-15 13:38:58 +1100

    Split main into a callable function for metadata merging; Various pep8 fixes.

--- yaml_merge.py
+++ yaml_merge.py
@@ -11,45 +11,102 @@ from __future__ import absolute_import
 import logging
 import os
 import uuid
+import copy
 import click
 import yaml
-import copy
 os.environ["CPL_ZIP_ENCODING"] = "UTF-8"
 
 def image_dict(target):
     """
-    Returns a datacube-compatible dictionary of TIF image paths 
+    Returns a datacube-compatible dictionary of TIF image paths
     """
-    nbar_match_dict = {'blue': 'B02', 'green': 'B03', 'red': 'B04', 'nir': 'B08', 'rededge1': 'B05',\
-                  'rededge2': 'B06', 'rededge3':'B07', 'rededge4': 'B8A',  'swir1': 'B11', 'swir2': 'B12',\
-                  'aerosol': 'B01', 'contiguity': 'CONTIGUITY', 'pixel_quality': 'QA'}     
-    nbart_match_dict = {'t-blue': 'B02', 't-green': 'B03', 't-red': 'B04', 't-nir': 'B08', 't-rededge1': 'B05',\
-                  't-rededge2': 'B06', 't-rededge3': 'B07', 't-rededge4': 'B8A', 't-swir1': 'B11', 't-swir2': 'B12',\
-                  't-aerosol': 'B01', 't-contiguity': 'CONTIGUITY'}
-    file_list = []
-    for file in os.listdir(target):
-        if '.TIF' in file:
-            file_list.append(os.path.join(os.path.abspath(target), file))        
-  
-    nbar_target = os.path.join(target, 'NBAR')
+    nbar_match_dict = {'blue': 'B02',
+                       'green': 'B03',
+                       'red': 'B04',
+                       'nir': 'B08',
+                       'rededge1': 'B05',
+                       'rededge2': 'B06',
+                       'rededge3': 'B07',
+                       'rededge4': 'B8A',
+                       'swir1': 'B11',
+                       'swir2': 'B12',
+                       'aerosol': 'B01',
+                       'contiguity': 'CONTIGUITY'}
+
+    nbart_match_dict = {'t-blue': 'B02',
+                        't-green': 'B03',
+                        't-red': 'B04',
+                        't-nir': 'B08',
+                        't-rededge1': 'B05',
+                        't-rededge2': 'B06',
+                        't-rededge3': 'B07',
+                        't-rededge4': 'B8A',
+                        't-swir1': 'B11',
+                        't-swir2': 'B12',
+                        't-aerosol': 'B01',
+                        't-contiguity': 'CONTIGUITY'}
+
+    pq_match_dict = {'pixel_quality': 'QA'}
 
     img_dict = {}
+
+    for file in os.listdir(target):
+        if '.TIF' in file:
+            for band_label, band_name in pq_match_dict.items():
+                if band_name in file:
+                    fname = os.path.join(target, file)
+                    img_dict[band_label] = {'path': fname, 'layer': 1}
+
+    nbar_target = os.path.join(target, 'NBAR')
     for file in os.listdir(nbar_target):
         if '.TIF' in file:
-            for band_label in nbar_match_dict.keys():
-                if nbar_match_dict[band_label] in file:
-                    img_dict[band_label] = {'path': os.path.join(nbar_target, file), 'layer': 1}
-            path = os.path.join(nbar_target, file)      
-            file_list.append(str(path))
-    file_list = []
-    nbart_target = os.path.join(target,'NBART')
+            for band_label, band_name in nbar_match_dict.items():
+                if band_name in file:
+                    fname = os.path.join(nbar_target, file)
+                    img_dict[band_label] = {'path': fname, 'layer': 1}
+
+    nbart_target = os.path.join(target, 'NBART')
     for file in os.listdir(nbart_target):
-         if '.TIF' in file:
-            for band_label in nbart_match_dict.keys():
-                if nbart_match_dict[band_label] in file:
-                    img_dict[band_label] = {'path': os.path.join(nbart_target, file), 'layer': 1} 
-            file_list.append(str(path))
+        if '.TIF' in file:
+            for band_label, band_name in nbart_match_dict.items():
+                if band_name in file:
+                    fname = os.path.join(nbart_target, file)
+                    img_dict[band_label] = {'path': fname, 'layer': 1}
+
     return img_dict
+
+
+def merge_metadata(level1_tags, gaip_tags, package_dir):
+    """
+    Combine the metadata from input sources and output
+    into a single ARD metadata yaml.
+    """
+    # TODO: extend yaml document to include fmask and gqa yamls
+    # Merge source into yaml - add UUID
+    tags = copy.deepcopy(level1_tags)
+    merged_yaml = {
+        'algorithm_information': gaip_tags['algorithm_information'],
+        'software_versions': gaip_tags['software_versions'],
+        'source_data': gaip_tags['source_data'],
+        'system_information': gaip_tags['system_information'],
+        'id': str(uuid.uuid4()),
+        'processing_level': 'Level-2',
+        'product_type': 'S2MSIARD',
+        'platform': level1_tags['platform'],
+        'instrument': level1_tags['instrument'],
+        'format': {'name': 'GeoTIFF'},
+        'tile_id': level1_tags['tile_id'],
+        'extent': level1_tags['extent'],
+        'grid_spatial': level1_tags['grid_spatial'],
+        'image': {
+            'tile_reference': level1_tags['image']['tile_reference'],
+            'cloud_cover_percentage': level1_tags['image']['cloud_cover_percentage'],
+            'bands': image_dict(package_dir)},
+        'lineage': {'source_datasets': tags},
+        }
+
+    return merged_yaml
+
 
 def merge(target_yaml, source_root):
     """
@@ -60,39 +117,23 @@ def merge(target_yaml, source_root):
         root_dir = os.path.dirname(target['source_data']['source_level1'])
         root_dir = os.path.basename(root_dir)
         basename = os.path.basename(target['source_data']['source_level1'])
-    source_yaml = os.path.join(source_root, os.path.join(source_root,root_dir,basename+".yaml"))
+
+    source_yaml = os.path.join(source_root,
+                               os.path.join(source_root,
+                                            root_dir,
+                                            basename + ".yaml"))
     target_root = os.path.dirname(target_yaml)
     target_basename = os.path.basename(target_root)
-    granule = target_basename.replace('ARD', 'L1C')  
+    granule = target_basename.replace('ARD', 'L1C')
     for document in yaml.load_all(open(source_yaml)):
         if granule == document['tile_id']:
             source = document
-    # Merge source into yaml - add UUID
-    new_source = copy.deepcopy(source)
-    merged_yaml = {
-            'algorithm_information': target['algorithm_information'],
-            'software_versions': target['software_versions'],
-            'source_data': target['source_data'],
-            'system_information': target['system_information'],         
-            'id': str(uuid.uuid4()),
-            'processing_level': 'Level-2',
-            'product_type': 'S2MSIARD',
-            'platform': source['platform'],
-            'instrument': source['instrument'],
-            'format': {'name': 'GeoTIFF'},
-            'tile_id': source['tile_id'],
-            'extent': source['extent'],
-            'grid_spatial': source['grid_spatial'],
-            'image': {
-                    'tile_reference': source['image']['tile_reference'],
-                    'cloud_cover_percentage': source['image']['cloud_cover_percentage'],
-                    'bands': image_dict(target_root)},
-            'lineage': {'source_datasets': new_source},
-            }
+
+    merged_yaml = merge_metadata(source, target, target_root)
 
     return merged_yaml
 
-    
+
 @click.command(help=__doc__)
 @click.argument('targets',
                 type=click.Path(exists=True, readable=True, writable=False),
@@ -102,7 +143,7 @@ def merge(target_yaml, source_root):
 def main(targets, source):
     """
     For each yaml in input 'targets' update it's content to include the level 1 soruce content available at
-    'source' and overwrite the input yaml 
+    'source' and overwrite the input yaml
     """
     logging.basicConfig(format='%(asctime)s %(levelname)s %(message)s', level=logging.INFO)
     for target_yaml in targets:
