31cecb174bdb0efcce7cb9565d7c48a706e96f6d
Josh Sixsmith <josh.sixsmith@gmail.com>
2017-12-18 11:15:14 +1100

    Added __init__.py, MANIFEST.in, setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(name='s2pkg',
+      version='0.0.1',
+      description=('A temporary solution to get packaging underway. '
+                   'Code will eventually be ported eo-datasets.'),
+      packages=['s2pkg'],
+      include_package_data=True)
