60736abf5d3bafd1067ecc12ce4f48c6158f66ed
Josh Sixsmith <josh.sixsmith@gmail.com>
2018-01-20 15:56:09 +1100

    Added string index for band id retrieval.

--- s2pkg/fmask_cophub.py
+++ s2pkg/fmask_cophub.py
@@ -47,7 +47,8 @@ def prepare_dataset(path, acq_parser_hint=None):
                                                   only_supported_bands=False)
 
             for acq in acqs:
-                image_dict[Path(acq.uri).stem] = {'path': acq.uri, 'layer': '1'}
+                image_dict[Path(acq.uri).stem[-3:]] = {'path': acq.uri,
+                                                       'layer': '1'}
 
             tasks.append(tuple([image_dict, granule_id, acq.granule_xml]))
 
