ff325360551779485d7e21270becf3acf49042e3
Josh Sixsmith <josh.sixsmith@gmail.com>
2018-01-22 18:08:45 +1100

    Fixed issue regarding indexing of the returned fmask prepare_dataset task, for processing single granules.

--- s2pkg/workflow.py
+++ s2pkg/workflow.py
@@ -122,7 +122,7 @@ class Package(luigi.Task):
     def requires(self):
         # task items for fmask
         ftask = prepare_dataset(self.level1, self.acq_parser_hint,
-                                self.granule)
+                                self.granule)[0]
 
         tasks = {'wagl': DataStandardisation(self.level1, self.workdir,
                                              self.granule),
