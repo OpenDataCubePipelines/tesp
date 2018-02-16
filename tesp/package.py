3da5fdfd4a35c2880cbd7041c4e268c82689d858
Josh Sixsmith <josh.sixsmith@gmail.com>
2018-02-16 11:24:57 +1100

    Minor cleanup.

--- tesp/package.py
+++ tesp/package.py
@@ -83,11 +83,7 @@ def wagl_unpack(scene, granule, h5group, outdir):
             fname = '{}{}_{}{}'.format(match_dict.get('prefix'), product,
                                        match_dict.get('band_name'),
                                        match_dict.get('extension'))
-            out_fname = pjoin(outdir,
-                              # base_dir.replace('L1C', 'ARD'),
-                              # granule.replace('L1C', 'ARD'),
-                              product,
-                              fname.replace('L1C', 'ARD'))
+            out_fname = pjoin(outdir, product, fname.replace('L1C', 'ARD'))
 
             # output
             if not exists(dirname(out_fname)):
