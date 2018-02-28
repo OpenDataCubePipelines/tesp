7116b0ed07b6c5b1bec37d5ebda694bb042615ba
Josh Sixsmith <josh.sixsmith@gmail.com>
2018-02-28 17:51:32 +1100

    Output package directory is now split via yyyy-mm-dd format.

--- tesp/workflow.py
+++ tesp/workflow.py
@@ -169,8 +169,9 @@ class ARDP(luigi.WrapperTask):
             container = acquisitions(level1, self.acq_parser_hint)
             for granule in container.granules:
                 work_dir = container.get_root(work_root, granule=granule)
-                # TODO; pkgdir for landsat data
-                pkgdir = pjoin(self.pkgdir, basename(dirname(level1)))
+                acq = container.get_acquisitions(None, granule, False)
+                ymd = acq.acquisition_datetime.strftime('%Y-%m-%d')
+                pkgdir = pjoin(self.pkgdir, ymd)
                 yield Package(level1, work_dir, granule, pkgdir)
 
 
