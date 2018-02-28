08db9d41ec4dd734afb42472baef4180ddd1357e
Josh Sixsmith <josh.sixsmith@gmail.com>
2018-02-28 18:30:49 +1100

    Changed s3_root to url_root to imply a generic http url path.

--- tesp/workflow.py
+++ tesp/workflow.py
@@ -122,7 +122,7 @@ class Package(luigi.Task):
     workdir = luigi.Parameter()
     granule = luigi.Parameter(default=None)
     pkgdir = luigi.Parameter()
-    s3_root = luigi.Parameter()
+    url_root = luigi.Parameter()
     yamls_dir = luigi.Parameter()
     cleanup = luigi.BoolParameter()
     acq_parser_hint = luigi.Parameter(default=None)
@@ -146,7 +146,7 @@ class Package(luigi.Task):
     def run(self):
         inputs = self.input()
         package(self.level1, inputs['wagl'].path, inputs['fmask'].path,
-                self.yamls_dir, self.pkgdir, self.s3_root, self.granule,
+                self.yamls_dir, self.pkgdir, self.url_root, self.granule,
                 self.acq_parser_hint)
 
         if self.cleanup:
@@ -163,7 +163,7 @@ class ARDP(luigi.WrapperTask):
     level1_list = luigi.Parameter()
     workdir = luigi.Parameter()
     pkgdir = luigi.Parameter()
-    s3_root = luigi.Parameter()
+    url_root = luigi.Parameter()
     acq_parser_hint = luigi.Parameter(default=None)
     s3_root = luigi.Parameter()
 
