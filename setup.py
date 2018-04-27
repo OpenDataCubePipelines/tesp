258e2d92213ac66cbcbb5b8e34447efff35cf5d7
Josh Sixsmith <josh.sixsmith@gmail.com>
2018-04-27 10:13:19 +1000

    Bump version.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup
 
 setup(name='tesp',
-      version='0.0.1',
+      version='0.0.4',
       description=('A temporary solution to get packaging underway. '
                    'Code will eventually be ported eo-datasets.'),
       packages=['tesp'],
