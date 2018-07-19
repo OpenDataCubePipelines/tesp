b5887fecefa8adda1803e7e789d2945edae78214
Alex Vincent <2034948+ASVincent@users.noreply.github.com>
2018-07-19 11:06:37 +1000

    Adding provider reference to metadata

--- tesp/yaml_merge.py
+++ tesp/yaml_merge.py
@@ -17,6 +17,30 @@ import yaml
 os.environ["CPL_ZIP_ENCODING"] = "UTF-8"
 
 
+def provider_reference_info(granule, wagl_tags):
+    """
+    Extracts provider reference metadata
+    Supported platforms are:
+        * LANDSAT
+        * SENTINEL2
+    :param granule:
+        A string referring to the name of the capture
+    
+    :return: 
+        Dictionary; contains satellite reference if identified
+    """
+    provider_info = {}
+    matches = None
+    if 'LANDSAT' in wagl_tags['source_datasets']['platform_id']:
+        matches = re.match(r'L\w\d(?P<reference_code>\d{6}).*', granule)
+    elif 'SENTINEL2' in wagl_tags['source_datasets']['platform_id']:
+        matches = re.match(r'.*_T(?P<reference_code>\d{1,2}[A-Z]{3})_.*')
+    
+    if matches:
+        provider_info.update(**matches.groupdict())
+    return provider_info
+
+
 def merge_metadata(level1_tags, wagl_tags, granule, image_paths):
     """
     Combine the metadata from input sources and output
@@ -30,6 +54,7 @@ def merge_metadata(level1_tags, wagl_tags, granule, image_paths):
              'SENTINEL_2B': 'S2MSIARD'}
 
     source_tags = {level1_tags['product_type']: copy.deepcopy(level1_tags)}
+    provider_info = provider_refernce_info(granule, wagl_tags)
 
     # TODO: extend yaml document to include fmask and gqa yamls
     # Merge tags from each input and create a UUID
@@ -46,11 +71,14 @@ def merge_metadata(level1_tags, wagl_tags, granule, image_paths):
         'tile_id': granule,
         'extent': level1_tags['extent'],
         'grid_spatial': level1_tags['grid_spatial'],
-        'image': {
-            'bands': image_paths},
+        'image': {'bands': image_paths},
         'lineage': {
             'ancillary': wagl_tags['ancillary'],
-            'source_datasets': source_tags},
-        }
+            'source_datasets': source_tags
+        },
+    }
+
+    if provider_info:
+        merged_yaml['provider'] = provider_info
 
     return merged_yaml
