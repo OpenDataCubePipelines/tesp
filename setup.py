e5b5d9755a4dc55d905d35cd2ab0d7a4ef5dcc3b
Alex Vincent <2034948+ASVincent@users.noreply.github.com>
2018-08-01 15:55:05 +1000

    Fixes to level-1 md generation; s3 contrib install and ARD file naming

--- setup.py
+++ setup.py
@@ -2,6 +2,10 @@
 
 from setuptools import setup, find_packages
 
+# There is a bug in the 2.7.6 release of luigi in the luigi.contrib.s3 module
+# Using an unreleased version until it is packaged and released
+
+
 setup(name='tesp',
       version='0.0.5',
       description=('A temporary solution to get packaging underway. '
@@ -27,7 +31,8 @@ setup(name='tesp',
       ],
       dependency_links=[
           'git+https://github.com/GeoscienceAustralia/eo-datasets@develop#egg=eodatasets-0.1dev',
-          'git+https://github.com/OpenDataCubePipelines/eugl.git#egg=eugl-0.0.2'
+          'git+https://github.com/OpenDataCubePipelines/eugl.git#egg=eugl-0.0.2',
+          'git+https://github.com/spotify/luigi.git@f9a99dce22e2887406c6d156d5d669660547d257#egg=luigi-2.7.7'
       ],
       scripts=['bin/s2package', 'bin/ard_pbs', 'bin/search_s2'],
       include_package_data=True)
