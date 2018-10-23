a1a49b532f60b915f548738bce3ce5498a57d35d
Alex Vincent <2034948+ASVincent@users.noreply.github.com>
2018-10-23 13:30:31 +1100

    Feature/s2 level1 generation (#40)
    * Adding level1 generation to s2_incremental file for testing
    
    * Adding level1 generation to s2_incremental file for testing
    
    * Modifications with testing
    
    * Committing versioning change and incremental updates to operational scripts
    
    * Updated scripts to be more nci-friendly
    
    * Add build and build setup

--- setup.py
+++ setup.py
@@ -1,13 +1,12 @@
 #!/usr/bin/env python
 
 from setuptools import setup, find_packages
-
-# There is a bug in the 2.7.6 release of luigi in the luigi.contrib.s3 module
-# Using an unreleased version until it is packaged and released
+import versioneer
 
 
 setup(name='tesp',
-      version='0.0.5',
+      version=versioneer.get_version(),
+      cmdclass=versioneer.get_cmdclass(),
       description=('A temporary solution to get packaging underway. '
                    'Code will eventually be ported eo-datasets.'),
       packages=find_packages(),
@@ -18,7 +17,7 @@ setup(name='tesp',
           'folium',
           'geopandas',
           'h5py',
-          'luigi',
+          'luigi>2.7.6',
           'numpy',
           'pathlib',
           'pyyaml',
@@ -26,9 +25,10 @@ setup(name='tesp',
           'scikit-image',
           'shapely',
           'structlog',
-          'eodatasets',
           'checksumdir',
+          'eodatasets',
           'eugl',
+          'wagl',
       ],
       extras_require=dict(
           test=[
@@ -40,12 +40,13 @@ setup(name='tesp',
           ],
       ),
       dependency_links=[
-          'git+https://github.com/GeoscienceAustralia/eo-datasets@develop#egg=eodatasets-0.1dev',
-          'git+https://github.com/OpenDataCubePipelines/eugl.git#egg=eugl-0.0.2',
-          'git+https://github.com/spotify/luigi.git@f9a99dce22e2887406c6d156d5d669660547d257#egg=luigi-2.7.7'
+          'git+https://github.com/GeoscienceAustralia/eo-datasets.git@develop#egg=eodatasets',
+          'git+https://github.com/GeoscienceAustralia/wagl@master#egg=wagl',
+          'git+https://github.com/OpenDataCubePipelines/eugl.git@master#egg=eugl',
       ],
+
       scripts=['bin/s2package',
                'bin/ard_pbs',
                'bin/search_s2',
-               'bin/s2_incremental'],
+               'bin/s2-nci-processing'],
       include_package_data=True)
