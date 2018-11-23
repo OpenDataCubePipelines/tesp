87b7e6326844be8095ec8986cba63875b0f853a1
passangd <41933342+passangd@users.noreply.github.com>
2018-11-23 12:44:16 +1100

    Collection upgrade feedback (#44)
    * software version metadata
    
    * invoke the imported functions
    
    * improve gqa version info collection
    
    * changes to address the ard collection upgrade enchangement address
    
    * Changes to standardize overview tile size for USGS collection upgrade
    
    * remove code to make gqa optional

--- tesp/workflow.py
+++ tesp/workflow.py
@@ -144,6 +144,7 @@ class Package(luigi.Task):
 
     def run(self):
         inputs = self.input()
+
         package(self.level1, inputs['wagl'].path, inputs['fmask'].path,
                 inputs['gqa'].path, self.yamls_dir, self.pkgdir, self.granule,
                 self.products, self.acq_parser_hint)
--- tesp/yaml_merge.py
+++ tesp/yaml_merge.py
@@ -14,7 +14,7 @@ import os
 import uuid
 import copy
 import re
-
+import numpy as np
 import click
 import yaml
 import fmask
@@ -48,7 +48,7 @@ def provider_reference_info(granule, wagl_tags):
     return provider_info
 
 
-def merge_metadata(level1_tags, wagl_tags, gqa_tags, granule, image_paths):
+def merge_metadata(level1_tags, wagl_tags, gqa_tags, granule, image_paths, platform):
     """
     Combine the metadata from input sources and output
     into a single ARD metadata yaml.
@@ -77,6 +77,20 @@ def merge_metadata(level1_tags, wagl_tags, gqa_tags, granule, image_paths):
     software_versions['tesp'] = {'repo_url': tesp_repo_url,
                                  'version': tesp_version()}
 
+    # for Landsat, from_dt and to_dt in ARD-METADATA is populated from max and min timedelta values
+    if platform == 'LANDSAT':
+
+        center_dt = np.datetime64(level1_tags['extent'].pop('center_dt'))
+        from_dt = center_dt + np.timedelta64(int(float(wagl_tags.pop('timedelta_min')) * 1000000), 'us')
+        to_dt = center_dt + np.timedelta64(int(float(wagl_tags.pop('timedelta_max')) * 1000000), 'us')
+
+        level1_tags_extent = {'center_dt': '{}Z'.format(center_dt),
+                              'coord': level1_tags['extent'].pop('coord'),
+                              'from_dt': '{}Z'.format(from_dt),
+                              'to_dt': '{}Z'.format(to_dt)}
+    else:
+        level1_tags_extent = level1_tags['extent']
+
     # TODO: extend yaml document to include fmask and gqa yamls
     # Merge tags from each input and create a UUID
     merged_yaml = {
@@ -91,7 +105,7 @@ def merge_metadata(level1_tags, wagl_tags, gqa_tags, granule, image_paths):
         'gqa': gqa_tags,
         'format': {'name': 'GeoTIFF'},
         'tile_id': granule,
-        'extent': level1_tags['extent'],
+        'extent': level1_tags_extent,
         'grid_spatial': level1_tags['grid_spatial'],
         'image': {'bands': image_paths},
         'lineage': {
