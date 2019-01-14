8e68e01ed1aef301f887a7386424dbc38ceeada8
Alex Vincent <2034948+ASVincent@users.noreply.github.com>
2019-01-14 14:06:05 +1100

    Removing internal prepare references; moved to eo-datasets

--- tesp/prepare.py
+++ tesp/prepare.py
@@ -1,8 +1,8 @@
 from pathlib import Path
 
-from .ls_usgs_l1_prepare import prepare_dataset as landsat_prepare
-from .s2_prepare_cophub_zip import prepare_dataset as sentinel_2_zip_prepare
-from .s2_l1c_aws_pds_prepare import prepare_dataset as sentinel_2_aws_pds_prepare
+from eodatasets.prepare.ls_usgs_l1_prepare import prepare_dataset as landsat_prepare
+from eodatasets.prepare.s2_prepare_cophub_zip import prepare_dataset as sentinel_2_zip_prepare
+from eodatasets.prepare.s2_l1c_aws_pds_prepare import prepare_dataset as sentinel_2_aws_pds_prepare
 
 
 def extract_level1_metadata(acq, acquisition_path):
@@ -12,8 +12,11 @@ def extract_level1_metadata(acq, acquisition_path):
     """
     # Optional (not installed yet on Travis)
     # pytest: disable=import-error
-    from wagl.acquisition.sentinel import _Sentinel2SinergiseAcquisition, Sentinel2Acquisition
+    from wagl.acquisition.sentinel import (
+        _Sentinel2SinergiseAcquisition, Sentinel2Acquisition
+    )
     from wagl.acquisition.landsat import LandsatAcquisition
+
 
     if isinstance(acq, _Sentinel2SinergiseAcquisition):
         return sentinel_2_aws_pds_prepare(Path(acq.granule_xml))
