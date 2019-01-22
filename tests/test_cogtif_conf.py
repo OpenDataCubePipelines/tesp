08df9e1160ccd23d5c3896026b521e917c97af26
Alex Vincent <2034948+ASVincent@users.noreply.github.com>
2019-01-22 17:55:36 +1100

    Adding tests for options resolution

--- tests/test_cogtif_conf.py
+++ tests/test_cogtif_conf.py
@@ -1,23 +1,26 @@
-from unittest import TestCase
+from unittest import TestClass
 from collections import namedtuple
 
+import numpy
 from tesp.package import get_cogtif_options
 from deepdiff import DeepDiff
 
-TempDataset = namedtuple('TempDataset', ['shape', 'chunks'])
+TestAcquisition = namedtuple('TestAcquisiton', ['data'])
+TestDataset = namedtuple('TestDataset', ['shape', 'chunks'])
 
-
-class TestCogtifOptions(TestCase):
+class TestCogtifOptions(TestClass):
 
     @classmethod
     def create_acq(cls, shape, chunks):
-        return TempDataset(
-            shape=shape,
-            chunks=chunks
+        return TestAcquisition(
+            TestDataset(
+                shape=shape,
+                chunks=chunks
+            )
         )
 
     def test_cogtif_low_res(self):
-        low_res = self.create_acq((80, 80), (0, 0))
+        low_res = self.create_acq((80, 80), (0,0))
         expected = {
             'options': {
                 'compress': 'deflate',
@@ -31,13 +34,13 @@ class TestCogtifOptions(TestCase):
         self.assertEqual(DeepDiff(results, expected), {})
 
     def test_cogtif_boundary_res(self):
-        low_res = self.create_acq((1024, 512), (512, 512))
+        low_res = self.create_acq((1024, 512), (0,0))
         expected = {
             'options': {
                 'compress': 'deflate',
                 'zlevel': 4,
-                'copy_src_overviews': 'yes',
-                'blockysize': 512,
+                'copy_src_overviews': 'yes'
+                'blockysize': 80,
                 'blockxsize': 256
             },
             'config_options': {}
@@ -52,13 +55,12 @@ class TestCogtifOptions(TestCase):
             'options': {
                 'compress': 'deflate',
                 'zlevel': 4,
+                'copy_src_overviews': 'yes'
                 'blockysize': 512,
                 'blockxsize': 512,
-                'tiled': 'yes',
-                'copy_src_overviews': 'yes'
             },
             'config_options': {
-                'GDAL_TIFF_OVR_BLOCKSIZE': 512
+                'GDAL_TIFF_OVER_BLOCKSIZE': 512
             }
         }
 
@@ -71,10 +73,9 @@ class TestCogtifOptions(TestCase):
             'options': {
                 'compress': 'deflate',
                 'zlevel': 4,
-                'copy_src_overviews': 'yes',
+                'copy_src_overviews': 'yes'
                 'blockysize': 1024,
                 'blockxsize': 1024,
-                'tiled': 'yes'
             },
             'config_options': {}
         }
