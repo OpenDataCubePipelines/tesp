d44c4b31abdda7f9eb4e7e581aa3e9f53bc32530
Josh Sixsmith <josh.sixsmith@gmail.com>
2020-09-25 16:15:16 +1000

    Updated with black formatting.

--- tesp/luigi_db_utils.py
+++ tesp/luigi_db_utils.py
@@ -36,36 +36,45 @@ def retrieve_status(fname, task_name):
     tasks, events, params = read_task_db(fname)
 
     task = tasks[tasks.name == task_name]
-    l1_datasets = params[params.name == 'level1']
-    granules = params[params.name == 'granule']
-    l1_granules = l1_datasets.merge(granules, suffixes=('_level1', '_granule'),
-                                    on='task_id')
+    l1_datasets = params[params.name == "level1"]
+    granules = params[params.name == "granule"]
+    l1_granules = l1_datasets.merge(
+        granules, suffixes=("_level1", "_granule"), on="task_id"
+    )
 
     # event status for the DataStandardisation Task
-    status = task.merge(events, how='left', left_on='id', right_on='task_id',
-                        suffixes=['_{}'.format(task_name), '_events'])
+    status = task.merge(
+        events,
+        how="left",
+        left_on="id",
+        right_on="task_id",
+        suffixes=["_{}".format(task_name), "_events"],
+    )
 
     # final status for each DataStandardisation Task
-    final_status = status.drop_duplicates('id_{}'.format(task_name),
-                                          keep='last')
+    final_status = status.drop_duplicates("id_{}".format(task_name), keep="last")
 
     # get the DONE, FAILED & PENDING Tasks
     # (if the task status is PENDING:
     # then the compute job could've timed out
     # or
     # an upstream dependency failed for some reason
-    done = final_status[final_status.event_name == 'DONE']
-    fail = final_status[final_status.event_name == 'FAILED']
-    pending = final_status[final_status.event_name == 'PENDING']
-    running = final_status[final_status.event_name == 'RUNNING']
+    done = final_status[final_status.event_name == "DONE"]
+    fail = final_status[final_status.event_name == "FAILED"]
+    pending = final_status[final_status.event_name == "PENDING"]
+    running = final_status[final_status.event_name == "RUNNING"]
 
-    l1_done = done.merge(l1_granules, how='left', right_on='task_id',
-                         left_on='id_{}'.format(task_name))
-    l1_fail = fail.merge(l1_granules, how='left', right_on='task_id',
-                         left_on='id_{}'.format(task_name))
-    l1_pending = pending.merge(l1_granules, how='left', right_on='task_id',
-                               left_on='id_{}'.format(task_name))
-    l1_running = running.merge(l1_granules, how='left', right_on='task_id',
-                               left_on='id_{}'.format(task_name))
+    l1_done = done.merge(
+        l1_granules, how="left", right_on="task_id", left_on="id_{}".format(task_name)
+    )
+    l1_fail = fail.merge(
+        l1_granules, how="left", right_on="task_id", left_on="id_{}".format(task_name)
+    )
+    l1_pending = pending.merge(
+        l1_granules, how="left", right_on="task_id", left_on="id_{}".format(task_name)
+    )
+    l1_running = running.merge(
+        l1_granules, how="left", right_on="task_id", left_on="id_{}".format(task_name)
+    )
 
     return l1_done, l1_fail, l1_pending, l1_running
